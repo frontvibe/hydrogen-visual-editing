a8393c85f1c5283ed29190d7d0659b613a24a8cc
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2024-12-01 15:08:55 -0500

    Add vite

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 // Virtual entry point for the app
-import * as remixBuild from '@remix-run/dev/server-build';
+import * as remixBuild from 'virtual:remix/server-build';
 import {storefrontRedirect} from '@shopify/hydrogen';
 import {createRequestHandler} from '@shopify/remix-oxygen';
 import {createAppLoadContext} from '~/lib/context';
